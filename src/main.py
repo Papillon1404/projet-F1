71a38ae58d35eaca33f595727a7330dc8b97cc4c
Arthur Auillans <arthur.auillans@etu.minesparis.psl.eu>
2026-01-26 23:52:52 +0100

    animation ok probleme de discrétisation quand v petit

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from src.circuit import Circuit
 from src.simulator import LapSimulator
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
+from matplotlib.animation import FuncAnimation
+
 
 
 ROOT = Path(__file__).resolve().parent.parent
@@ -13,35 +16,27 @@ car = Car()
 circuit = Circuit(DATA / "circuit.json")
 
 sim = LapSimulator(car, circuit)
-x, v, lap_time, time_per_segment ,a = sim.simulate()
+t, x, v, lap_time, time_per_segment ,a = sim.simulate()
 v_min = np.min(v)
 v_max = np.max(v)
 
 
 X, Y = circuit.compute_xy(ds=1.0)
-
-plt.figure(figsize=(6,6))
-plt.plot(X, Y)
-plt.axis("equal")
-plt.show()
+# Distance curviligne du circuit
+ds_geom = np.sqrt(np.diff(X)**2 + np.diff(Y)**2)
+s_geom = np.insert(np.cumsum(ds_geom), 0, 0.0)
 
 
-from scipy.interpolate import interp1d
 
-s = np.linspace(0, x[-1], len(X))
-interp_x = interp1d(x, X, fill_value="extrapolate")
-interp_y = interp1d(x, Y, fill_value="extrapolate")
+interp_X = interp1d(s_geom, X, fill_value="extrapolate")
+interp_Y = interp1d(s_geom, Y, fill_value="extrapolate")
 
-car_X = interp_x(x)
-car_Y = interp_y(x)
+car_X = interp_X(x)
+car_Y = interp_Y(x)
 
-# print 
-print(f"Min speed: {v_min*3.6:.1f} km/h")
-print(f"Max speed: {v_max*3.6:.1f} km/h")
-print(f"Lap time: {lap_time:.2f} s")
-print(f"Time lost in slow zones: {np.sum(time_per_segment[v < 50]):.2f} s")
 
-# plot
+
+### plot ###########################################################################################
 fig, (ax_speed, ax_acc, ax_track) = plt.subplots(
     3, 1, figsize=(10, 8), gridspec_kw={"height_ratios": [2, 2, 3]}
 )
@@ -49,13 +44,13 @@ fig, (ax_speed, ax_acc, ax_track) = plt.subplots(
 
 # vitesse
 line_v, = ax_speed.plot([], [], lw=2)
-ax_speed.set_xlim(0, x[-1])
+ax_speed.set_xlim(0, 10000)
 ax_speed.set_ylim(0, v.max()*3.6*1.1)
 ax_speed.set_ylabel("Speed (km/h)")
 
 # accélération
 line_a, = ax_acc.plot([], [], color="red")
-ax_acc.set_xlim(0, x[-1])
+ax_acc.set_xlim(0, 10000)
 ax_acc.set_ylim(a.min()*1.2, a.max()*1.2)
 ax_acc.set_ylabel("Acceleration (m/s²)")
 
@@ -68,14 +63,17 @@ ax_track.set_title("Circuit Layout")
 
 
 def update(i):
-    line_v.set_data(x[:i], v[:i]*3.6)
-    line_a.set_data(x[:i], a[:i])
+    line_v.set_data(t[:i], v[:i]*3.6)
+    line_a.set_data(t[:i], a[:i])
 
-    car_dot.set_data(car_X[i], car_Y[i])
+    car_dot.set_data([car_X[i]], [car_Y[i]])
+
 
     return line_v, line_a, car_dot
 
 
+
+### animation ######################################################################################
 from matplotlib.animation import FuncAnimation
 
 ani = FuncAnimation(
@@ -90,3 +88,13 @@ plt.tight_layout()
 plt.show()
 
 
+
+
+# print 
+
+print(f"Min speed: {v_min*3.6:.1f} km/h")
+print(f"Max speed: {v_max*3.6:.1f} km/h")
+print(f"Lap time: {lap_time:.2f} s")
+print(f"Time lost in slow zones: {np.sum(time_per_segment[v < 50]):.2f} s")
+
+print(f":{float(t[0])}")
--- src/simulator.py
+++ src/simulator.py
@@ -2,18 +2,20 @@ import numpy as np
 import math
 
 class LapSimulator:
-    def __init__(self,car,circuit,mu=1.7,g=9.81,dx=1.0):
+    
+    def __init__(self,car,circuit,mu=1.7,g=9.81,dt_target=0.02):
         self.car = car
         self.circuit = circuit
         self.mu = mu
         self.g = g
-        self.dx = dx
+        self.dt_target = dt_target  # pas temporel cible
 
         self.x =[]
         self.v_max = []
 
         self._discreticize_circuit()
 
+    
     def _discreticize_circuit(self):
         pos = 0.0
 
@@ -40,24 +42,36 @@ class LapSimulator:
         self.v_max = np.array(self.v_max)
 
     def simulate(self):
+        x_sim = [0.0]           # position curviligne
+        v_sim = [1.0]           # vitesse initiale 1 m/s pour éviter dt énorme
+        dt_list = [0.0]         # temps cumulé initial
+        a_list = []             # accélération
+
+        
         v = np.zeros(len(self.x))
 
         #Passe avant : accélération
         for i in range(1,len(v)):
             a = self.car.acceleration(v[i-1])
-            v[i] = np.sqrt(max(0,v[i-1]**2 + 2 * a * self.dx))
+            dx = max(v[i-1] * self.dt_target, 0.01)
+            v[i] = np.sqrt(max(0,v[i-1]**2 + 2 * a * dx))
             v[i] = min(v[i], self.v_max[i])
+        
 
         #Passe arriere : freinage
         for i in reversed(range(len(v)-1)):
+            dx = max(v[i-1] * self.dt_target, 0.01)
             v_brake = np.sqrt(v[i+1]**2 + 2 * self.mu * self.g * self.dx)
             v[i] = min(v[i], v_brake)
 
         dt = self.dx / np.maximum(v, 1e-3)
         total_time = np.sum(dt)
 
-        time_per_segment = self.dx / np.maximum(v, 1e-3)
-        a = np.gradient(v, self.dx)
-        return self.x, v, total_time, time_per_segment, a
+
+        time_per_segment = dx / np.maximum(v, 1e-3)
+        t = np.cumsum(np.concatenate([[0], time_per_segment]))
+        a = np.gradient(v, time_per_segment)
+
+        return t, self.x, v, total_time, time_per_segment, a
     
     
